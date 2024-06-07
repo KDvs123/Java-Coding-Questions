6925fb08bf1956390d2d76bf295c717979846166
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-07 22:15:03 +0530

    java palindrome string solution

---
+++ palindrome.java
@@ -0,0 +1,12 @@
+public class palindrome{
+    public static void main(String[] args){
+        String word="Mom";
+        String newWord="";
+        for(int i=word.length()-1;i>=0;i--){
+            newWord+=word.chatAt(i);
+
+        }
+
+        System.out.println(word.equals(newWord)? "Palindrome":"Not Palindrome");
+    }
+}
