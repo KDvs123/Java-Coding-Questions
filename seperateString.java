f3457444ea5966585592b6f581cd28383d64b6a6
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-09 12:03:59 +0530

    Exercise solution for the seperate the individual characters from a string

---
+++ seperateString.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class seperateString {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String sentence=input.nextLine();
+        for(int i=0;i<sentence.length();i++){
+            System.out.print(sentence.charAt(i)+" ");
+        }
+
+    }
+
+    }
+
+
